b933b215406331fe6e1d87db50f5bfe1d7d93bbb
MW <hyoukim@naver.com>
2021-05-25 12:43:51 +0900

    travis yml and aws deploy added

--- backend/db.js
+++ backend/db.js
@@ -1,10 +1,16 @@
 const mysql = require("mysql");
 const pool = mysql.createPool({
     connectionLimit: 10,
-    host: 'mysql',
-    user: 'root',
-    password: 'test',
-    database: 'myapp'
+    // docker-compose 환경변수로 등록한 값을 가져오도록
+    // host: 'mysql',
+    host: process.env.MYSQL_HOST,
+    // user: 'root',
+    user: process.env.MYSQL_USER,
+    // password: 'test',
+    password: process.env.MYSQL_ROOT_PASSWORD,
+    // database: 'myapp'
+    database: process.env.MYSQL_DATABASE,
+    port: process.env.MYSQL_PORT
 });
 
 exports.pool = pool; // pool을 사용할 수 있게 exports
